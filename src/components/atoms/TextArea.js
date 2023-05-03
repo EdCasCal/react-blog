a628567fb1ecc2d37e8980218c086fd4f60b0b4a
Edel Castillo Calderon <edel.castillo.calderon@gmail.com>
2023-05-02 22:57:52 -0600

    Refined Contact Page separation of concerns.

--- src/components/atoms/TextArea.js
+++ src/components/atoms/TextArea.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./TextArea.module.css";
 
-const TextArea = ({ placeholder, onChange, className }) => {
+const TextArea = ({ placeholder, onChange, className, value }) => {
 	return (
 		<textarea
 			rows={5}
 			className={`${className ?? ""} ${styles.textAreaBaseStyle} `}
 			placeholder={placeholder}
+			value={value}
 			onChange={onChange}
 		/>
 	);
--- src/pages/Contact/ContactPage.js
+++ src/pages/Contact/ContactPage.js
@@ -1,31 +1,16 @@
 import React from "react";
+
 import styles from "./ContactPage.module.css";
-import Input from "../../components/atoms/Input";
-import Button from "../../components/atoms/Button";
-import TextArea from "../../components/atoms/TextArea";
+
+import ContactForm from "../../components/organisms/ContactForm";
 
 function ContactPage() {
 	return (
 		<section className={styles.contactContainer}>
 			<h1>Contact Us</h1>
-			<p className={styles.contactSubheader}>Feel free to reach out to us with any questions or feedback!</p>
+			<p className={styles.contactDescription}>Feel free to reach out to us with any questions or feedback!</p>
 
-			<form className={styles.formContent}>
-				<label className={styles.formLabel}>
-					Name
-					<Input type="text" name="name" placeholder={"John Doe"} className={styles.formInput} />
-				</label>
-				<label className={styles.formLabel}>
-					Email
-					<Input type="email" name="email" placeholder={"lorem@ipsum.com"} className={styles.formInput} />
-				</label>
-				<label className={styles.formLabel}>
-					Message
-					<TextArea name="message" placeholder={"message text"} className={styles.formInput} />
-				</label>
-
-				<Button type="submit" text={"Submit"} className={styles.formSubmit}></Button>
-			</form>
+			<ContactForm />
 		</section>
 	);
 }
